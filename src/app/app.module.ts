37050c338a790535a5ae276506cf613000f613b8
SharkSkeleton <mishamyagkiy3@gmail.com>
2022-08-07 20:16:02 +0300

    app.module cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { UpperMenuComponent } from './upper-menu/upper-menu.component';
-import { CardsComponent } from './cards/cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UpperMenuComponent,
-    CardsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
