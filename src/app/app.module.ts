73c480de5b89c3213f873301b1c54c0c5fb93e2e
SharkSkeleton <mishamyagkiy3@gmail.com>
2022-08-07 20:31:33 +0300

    displaying for upper menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UpperMenuComponent } from './price-tracker-site/upper-menu/upper-menu.component';
 import { BaseComponent } from './price-tracker-site/base/base.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { BaseComponent } from './price-tracker-site/base/base.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
